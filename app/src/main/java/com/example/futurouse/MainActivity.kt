1dc9f8c742d1316ca0dfbd3265155f2987f1ef85
dmsRosa <91638513+dmsRosa@users.noreply.github.com>
2022-11-21 18:37:25 +0000

    Thermostat done, just needs some adjustments, but we are missin the screen between the functionality and the rooms menu

--- app/src/main/java/com/example/futurouse/MainActivity.kt
+++ app/src/main/java/com/example/futurouse/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.futurouse
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
@@ -16,8 +21,13 @@ import com.skydoves.balloon.BalloonAnimation
 import com.skydoves.balloon.BalloonSizeSpec
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import me.tankery.lib.circularseekbar.CircularSeekBar
+import java.lang.Math.abs
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    var currTemp = 10f
+    var newTemp = 10f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +66,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         supportActionBar?.title = ""
         changeFragment(Home())
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -80,9 +91,68 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-
     fun changeFragment(frag: Fragment) {
         val fragment = supportFragmentManager.beginTransaction()
         fragment.replace(R.id.fragment_container, frag).commit()
+    }
+
+    fun thermostat(view: View){
+
+        setContentView(R.layout.thermostat_screen)
+
+
+        val fanImg = findViewById<ImageView>(R.id.imageView2);
+        val rotate = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        fanImg.animation = rotate
+
+        val currentTemperature = findViewById<TextView>(R.id.textView2)
+        val newTemperature = findViewById<TextView>(R.id.textView3)
+        val thermometer = findViewById<ImageView>(R.id.imageView1)
+        currentTemperature.text = "${currTemp.toInt()} ºC"
+        newTemperature.text = "${newTemp.toInt()} ºC"
+
+        var seekbar: CircularSeekBar? = findViewById(R.id.seekbar)
+        seekbar?.progress = currTemp
+        seekbar?.setOnSeekBarChangeListener(object : CircularSeekBar.OnCircularSeekBarChangeListener{
+            override fun onProgressChanged(
+                circularSeekBar: CircularSeekBar?,
+                progress: Float,
+                fromUser: Boolean
+            ) {
+                newTemp = progress
+
+                newTemperature.text = "${newTemp.toInt()} ºC"
+            }
+
+            override fun onStopTrackingTouch(seekBar: CircularSeekBar?) {
+                val dif = newTemp - currTemp
+                if(dif.toInt() != 0){
+                    if(dif > 0.0f) thermometer.setImageResource(R.drawable.thermometer_temperature_up)
+                    else thermometer.setImageResource(R.drawable.thermometer_temperature_down)
+                    thermometer.alpha = 255F
+                    startCounter(currentTemperature,thermometer)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: CircularSeekBar?) {
+
+            }
+        })
+    }
+
+    fun startCounter(textView : TextView,imageView: ImageView){
+        val timeStart = 5000L
+        val timeStep = 1000L
+        object : CountDownTimer(timeStart, timeStep) {
+            val dif = newTemp - currTemp
+            val step = dif/(timeStart/1000)
+            override fun onTick(l: Long) {
+                currTemp += step
+                textView.text = "${currTemp.toInt()} ºC"
+            }
+            override fun onFinish() {
+                imageView.alpha = 0F
+            }
+        }.start()
     }
 }

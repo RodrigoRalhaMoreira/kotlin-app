0b4f80f63f4d4272645809efef8e3cf50492fc46
tjFernandes <tj.fernandes@campus.fct.unl.pt>
2022-11-26 16:12:41 +0000

    Oven functionalities done, need to save state now

--- app/src/main/java/com/example/futurouse/oven/OvenActivity.kt
+++ app/src/main/java/com/example/futurouse/oven/OvenActivity.kt
@@ -2,6 +2,9 @@ package com.example.futurouse.oven
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationSet
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -10,12 +13,17 @@ import kotlinx.android.synthetic.main.oven_activity.*
 
 class OvenActivity : AppCompatActivity()  {
 
+    private lateinit var currentActiveTab: CardView
+
+    private var fragContainer = R.drawable.layout_border
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.oven_activity)
 
-        val backButton = findViewById<ImageView>(R.id.backButton)
+        currentActiveTab = modeButton
+        currentActiveTab.setCardBackgroundColor(Color.parseColor("#ff9e6f"))
 
         backButton.setOnClickListener { this.finish() }
 
@@ -25,11 +33,13 @@ class OvenActivity : AppCompatActivity()  {
         val programsFragment = OvenPrograms()
 
         supportFragmentManager.beginTransaction().apply {
+            ovenFragContainer.setBackgroundResource(fragContainer)
             replace(R.id.ovenFragContainer, modeFragment)
             commit()
         }
 
         modeButton.setOnClickListener {
+            ovenFragContainer.setBackgroundResource(fragContainer)
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.ovenFragContainer, modeFragment)
                 commit()
@@ -38,33 +48,74 @@ class OvenActivity : AppCompatActivity()  {
         }
 
         tempButton.setOnClickListener {
+            ovenFragContainer.setBackgroundResource(fragContainer)
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.ovenFragContainer, tempFragment)
                 commit()
             }
-            setActiveTab(modeButton)
+            setActiveTab(tempButton)
         }
 
         timerButton.setOnClickListener {
+            ovenFragContainer.setBackgroundResource(fragContainer)
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.ovenFragContainer, timerFragment)
                 commit()
             }
-            setActiveTab(modeButton)
+            setActiveTab(timerButton)
         }
 
         programsButton.setOnClickListener {
+            ovenFragContainer.setBackgroundResource(0)
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.ovenFragContainer, programsFragment)
                 commit()
             }
-            setActiveTab(modeButton)
+            setActiveTab(programsButton)
         }
+
+        flames.scale = 10f
+
+        onOffButton.setOnClickListener {
+            if (onOffButton.text.contains("ON")) {
+                onOffButton.text = "TURN OFF"
+
+                val fadeIn = AlphaAnimation(0f,1f)
+                fadeIn.duration = 1000
+
+                val animation = AnimationSet(false)
+                animation.addAnimation(fadeIn)
+
+                flames.animation = animation
+
+                flames.visibility = View.VISIBLE
+
+            }
+            else {
+                onOffButton.text = "TURN ON"
+
+                val fadeOut = AlphaAnimation(1f,0f)
+                fadeOut.duration = 500
+
+                val animation = AnimationSet(false)
+                animation.addAnimation(fadeOut)
+
+                flames.animation = animation
+
+                flames.visibility = View.INVISIBLE
+
+            }
+        }
+
+
+
 
     }
 
     private fun setActiveTab(button: CardView) {
-        //button.setBackgroundColor(Color.rgb())
+        currentActiveTab.setCardBackgroundColor(Color.parseColor("#FFFAC7"))
+        button.setCardBackgroundColor(Color.parseColor("#ff9e6f"))
+        currentActiveTab = button
     }
 
 
--- app/src/main/java/com/example/futurouse/oven/OvenPrograms.kt
+++ app/src/main/java/com/example/futurouse/oven/OvenPrograms.kt
@@ -1,7 +1,36 @@
 package com.example.futurouse.oven
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.futurouse.R
+import com.google.android.material.card.MaterialCardView
+import kotlinx.android.synthetic.main.fragment_oven_programs.*
 
 class OvenPrograms : Fragment(R.layout.fragment_oven_programs) {
+
+    var settings = arrayListOf<MaterialCardView>()
+    var checkedSetting: MaterialCardView? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        settings.addAll(arrayListOf(setting1, setting2, setting3, setting4))
+
+        this.setListeners()
+    }
+
+    private fun setListeners() {
+        for (setting in settings) {
+            setting.setOnLongClickListener {
+                if (checkedSetting != null) {
+                    checkedSetting!!.isChecked = false
+                }
+                setting.isChecked = !setting.isChecked
+                checkedSetting = setting
+                true
+            }
+        }
+    }
 }
+
